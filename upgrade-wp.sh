30cfd7815e7b48c73ea1a7ff382fc8077ce90d69
Kaspars Bulins <kasparsb@hotmail.com>
2019-01-13 21:51:00 +0000

    Updated upgrade-wp with ability to download themes

--- upgrade-wp.sh
+++ upgrade-wp.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+
 wget http://wordpress.org/latest.tar.gz
 tar -xvf latest.tar.gz
-mv wordpress/* wp
-rmdir wordpress
+
+if [ "$1" = "--onlythemes" ]; then
+    mv wordpress/wp-content/themes/* app/themes
+else
+    # delete existing wp core
+    if [ -d "wp" ]; then
+        find wp/. ! -name '.gitignore' -type f,d -exec rm -rf {} +
+    else
+        mkdir wp
+    fi
+
+    if [ "$1" = "--themes" ]; then
+        mv wordpress/wp-content/themes/* app/themes
+    fi
+
+    mv wordpress/* wp    
+fi
+
+rm -rf wordpress
 rm latest.tar.gz
